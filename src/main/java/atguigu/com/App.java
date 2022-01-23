9fffd88154f31375e251e3a46839a50095005cf6
TomDong <TomDong@qq.com>
2022-01-23 22:07:40 +0800

    first commit

---
+++ src/main/java/atguigu/com/App.java
@@ -0,0 +1,13 @@
+package atguigu.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
