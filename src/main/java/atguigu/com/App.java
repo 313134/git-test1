81ae2777af3cdb80f9d76f550913c643557202f6
TomDong <TomDong@qq.com>
2022-01-24 00:28:43 +0800

    master commit4

--- src/main/java/atguigu/com/App.java
+++ src/main/java/atguigu/com/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println("hot-fix test");
         System.out.println("11111");
         System.out.println("12123");
+        System.out.println("212121212121");
     }
 }
